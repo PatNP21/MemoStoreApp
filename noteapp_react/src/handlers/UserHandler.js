64504a9c348bfc9a16d311b38abec3dd23820d01
patnajda99servers <mxk615@gmail.com>
2023-10-05 23:48:38 +0200

    new_commit_required

--- noteapp_react/src/handlers/UserHandler.js
+++ noteapp_react/src/handlers/UserHandler.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export class UserHandler {
 
-    baseURL = 'http://localhost:2000'
+    baseURL = 'https://memo-store-45jiz3qf2-patnp21.vercel.app:2000'
 
     registerNewUser(data) {
         return axios.post(`${this.baseURL}/registerUser/`, data)
